e508080026fc6c504c6f31372a0f73a6f1ef3039
Pavel Smirnov <mebelsibiri15@gmail.com>
2018-11-23 18:12:06 +0700

    first commit

---
+++ DatabaseEntities/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DatabaseEntities
+{
+    public class Image: BaseEntity
+    {
+        [Required]
+        public int ID { get; set; }
+
+        [Required]
+        public string URL { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime DateTimeAdded { get; set; }
+
+        [Required]
+        public int ID_User { get; set; }
+    }
+}
---
+++ DatabaseEntities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace Models.DatabaseEntities
+{
+    public class User: BaseEntity
+    {
+        //[Key]
+        public int ID { get; set; }
+
+        //[Required]
+        public string Name { get; set; }
+
+        //[Required]
+        public string Password { get; set; }
+
+        public Byte[] Photo { get; set; }
+
+        //[Required]
+        public string Email { get; set; }
+
+        public bool Joined { get; set; }
+    }
+}
