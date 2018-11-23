a906e9426aa4e6a136fc5d9a03b12114dd629869
ArtemSlesarenko <aslesarenko@coderlink.cc>
2018-11-23 11:17:45 +0300

    Перенес из основного проекта логику получения данных для примера. Добавил логирование в пример получения данных.

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,44 +1,32 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Models.Data;
+using Models.Logs;
 
 namespace Localization.Controllers
 {
+    /**
+     * Класс, иллюстрирующий возможность получения данных в Angular из VS
+     */
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
+        private LogTools lt;
+
+        public SampleDataController()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+            lt = new LogTools();
+        }
 
         [HttpGet("[action]")]
         public IEnumerable<WeatherForecast> WeatherForecasts()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                DateFormatted = DateTime.Now.AddDays(index).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            });
-        }
+            lt.WriteDebug("WeatherForecasts-->");
 
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
+            WeatherForecast wf = new WeatherForecast();
 
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
+            lt.WriteDebug("WeatherForecasts--<");
+            return wf.getData();
         }
     }
 }
