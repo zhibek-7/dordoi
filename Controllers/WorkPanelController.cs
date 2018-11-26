8bc4948a4f207356785afa0291133eebea37081a
Pavel <mebelsibiri15@gmail.com>
2018-11-26 13:56:47 +0700

    Работа с webApi

--- Controllers/WorkPanelController.cs
+++ Controllers/WorkPanelController.cs
@@ -16,6 +16,7 @@ namespace Localization.WebApi.work_panel
     {
         PostgreSqlEFContext context;
         private readonly UserRepository userRepository = new UserRepository();
+        private readonly StringRepository stringRepository = new StringRepository();
 
         public WorkPanelController(PostgreSqlEFContext context)
         {
@@ -25,6 +26,9 @@ namespace Localization.WebApi.work_panel
         [HttpGet]
         public IEnumerable<User> GetUsers()
         {
+            //List<Models.DatabaseEntities.String> strings = context.Strings.ToList();
+            List<Models.DatabaseEntities.String> strings = stringRepository.GetAll().ToList(); // получаем через нативный sql
+            Models.DatabaseEntities.String foundedString = stringRepository.GetByID(5);
             List<User> users = context.Users.ToList(); //получаем через EF
             users = userRepository.GetAll().ToList(); // получаем через нативный sql
             return users;
---
+++ WebApi/StringController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DAL.Context; // EF
+using DAL.Reposity.PostgreSqlRepository; // Native
+using Models.DatabaseEntities;
+
+namespace Localization.WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        private readonly StringRepository stringRepository = new StringRepository();
+
+        public StringController()
+        {
+            
+        }
+
+        [HttpGet]
+        public List<Models.DatabaseEntities.String> GetStrings()
+        {
+            List<Models.DatabaseEntities.String> strings = stringRepository.GetAll().ToList();
+            return strings;
+        }
+
+        [HttpGet("{id}")]
+        public Models.DatabaseEntities.String GetStringById(int id)
+        {
+            Models.DatabaseEntities.String foundedString = stringRepository.GetByID(id);
+            return foundedString;
+        }
+    }
+}
