b8232efc7c7be8f2409f182baec97f9f2bacb93b
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-11-29 10:32:05 +0400

    добавлена модель глоссария из бд

---
+++ Models/DatabaseEntities/Glossary.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.DatabaseEntities
+{
+    public class Glossary : BaseEntity
+    {
+
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int ID_File { get; set; }
+
+    }
+}
