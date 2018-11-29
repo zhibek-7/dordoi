00e19b8c5b3d5ee1cbc28a194e307dac24b6ed1b
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-11-29 10:35:11 +0400

    добавлена реализация методов GetByID, Update, AddNewTerm для GlossaryRepository

--- Dal/Reposity/PostgreSqlRepository/GlossaryRepository.cs
+++ Dal/Reposity/PostgreSqlRepository/GlossaryRepository.cs
@@ -34,7 +34,13 @@ namespace DAL.Reposity.PostgreSqlRepository
 
         public Glossary GetByID(int id)
         {
-            throw new NotImplementedException();
+            using (var dbConnection = this._context.Connection)
+            {
+                dbConnection.Open();
+                var glossary = dbConnection.QueryFirst<Glossary>("SELECT * FROM \"Glossaries\" WHERE \"ID\" = @GlossaryId", new { GlossaryId = id });
+                dbConnection.Close();
+                return glossary;
+            }
         }
 
         public void Remove(int id)
@@ -44,7 +50,15 @@ namespace DAL.Reposity.PostgreSqlRepository
 
         public void Update(Glossary item)
         {
-            throw new NotImplementedException();
+            using (var dbConnection = this._context.Connection)
+            {
+                dbConnection.Open();
+                dbConnection.Execute(
+                    sql: "UPDATE \"Glossaries\" SET \"Name\"=@Name, \"Description\"=@Description, \"ID_File\"=@ID_File " +
+                    "WHERE \"ID\"=@ID",
+                    param: item);
+                dbConnection.Close();
+            }
         }
 
         public IEnumerable<Models.DatabaseEntities.String> GetAssotiatedTermsByGlossaryId(int id)
@@ -60,6 +74,33 @@ namespace DAL.Reposity.PostgreSqlRepository
                             param: new { GlossaryId = id });
                 dbConnection.Close();
                 return terms;
+            }
+        }
+
+        public void AddNewTerm(int glossaryId, Models.DatabaseEntities.String newTerm)
+        {
+            var glossary = this.GetByID(id: glossaryId);
+            newTerm.ID_FileOwner = glossary.ID_File;
+            using (var dbConnection = this._context.Connection)
+            {
+                dbConnection.Open();
+                var idOfNewTerm = dbConnection
+                    .ExecuteScalar<int>(
+                        sql: "INSERT INTO \"Strings\" " +
+                        "(\"SubstringToTranslate\", \"Description\", \"Context\", " +
+                        "\"TranslationMaxLength\", \"ID_FileOwner\", \"PositionInFile\", " +
+                        "\"OriginalString\", \"HasTranslationSubstring\", \"TranslationSubstring\", " +
+                        "\"TranslationSubstringPositionInLine\") VALUES " +
+                        "(@SubstringToTranslate, @Description, @Context, @TranslationMaxLength, " +
+                        "@ID_FileOwner, @PositionInFile, @OriginalString, @HasTranslationSubstring, " +
+                        "@TranslationSubstring, @TranslationSubstringPositionInLine) " +
+                        "RETURNING \"ID\"",
+                        param: newTerm);
+                dbConnection
+                    .Execute(
+                        sql: "INSERT INTO \"GlossariesStrings\" (\"ID_Glossary\", \"ID_String\") VALUES (@GlossaryId, @StringId)",
+                        param: new { GlossaryId = glossaryId, StringId = idOfNewTerm });
+                dbConnection.Close();
             }
         }
 
