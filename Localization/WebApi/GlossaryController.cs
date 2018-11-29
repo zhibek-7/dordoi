7e0990e21949b4a93d0efc42474e8bab87f233b6
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-11-29 10:36:26 +0400

    GlossaryController добавлен к web api

---
+++ Localization/WebApi/GlossaryController.cs
@@ -0,0 +1,51 @@
+﻿using DAL.Reposity.PostgreSqlRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models.DatabaseEntities;
+using System.Collections.Generic;
+
+namespace Localization.WebApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GlossaryController : ControllerBase
+    {
+
+        private readonly GlossaryRepository _glossaryRepository;
+
+        public GlossaryController()
+        {
+            this._glossaryRepository = new GlossaryRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Glossary> GetAll()
+        {
+            return this._glossaryRepository.GetAll();
+        }
+
+        [HttpPut("{id}")]
+        public void Update(int id, [FromBody] Glossary updatedGlossary)
+        {
+            this._glossaryRepository.Update(updatedGlossary);
+        }
+
+        [HttpGet("{id}")]
+        public Glossary GetGlossaryById(int id)
+        {
+            return this._glossaryRepository.GetByID(id: id);
+        }
+
+        [HttpGet("{id}/get_assotiated_terms")]
+        public IEnumerable<Models.DatabaseEntities.String> GetAssotiatedTerms(int id)
+        {
+            return this._glossaryRepository.GetAssotiatedTermsByGlossaryId(id: id);
+        }
+
+        [HttpPost("{id}/add_term")]
+        public void AddTerm(int id, [FromBody] Models.DatabaseEntities.String newTerm)
+        {
+            this._glossaryRepository.AddNewTerm(glossaryId: id, newTerm: newTerm);
+        }
+
+    }
+}
