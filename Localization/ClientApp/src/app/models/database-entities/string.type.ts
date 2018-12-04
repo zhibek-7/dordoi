83a83891fe77b2bb2e16b8a3a59d902f11be4c9a
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-12-04 14:01:38 +0400

    клиентская модель строки обновлена

--- Localization/ClientApp/src/app/models/database-entities/string.type.ts
+++ Localization/ClientApp/src/app/models/database-entities/string.type.ts
@@ -1,42 +1,14 @@
 export class String {
 
-    id?: number;
-    substringToTranslate?: string;
-    description?: string;
-    context?: string;
-    id_fileOwner?: number;
-    positionInFile?: number;
-    originalString?: string;
-    hasTranslationSubstring?: boolean;
-    translationSubstring?: string;
-    translationMaxLength?: number;
-    translationSubstringPositionInLine?: number;
+  constructor(
+    public id?: number,
+    public substringToTranslate?: string,
+    public description?: string,
+    public context?: string,
+    public id_fileOwner?: number,
+    public translationMaxLength?: number,
+    public value?: string,
+    public positionInFullText?: number,
+  ) { }
 
-    constructor(
-        id?: number,
-        substringToTranslate?: string,
-        description?: string,
-        context?: string,
-        id_fileOwner?: number,
-        positionInFile?: number,
-        originalString?: string,
-        hasTranslationSubstring?: boolean,
-        translationSubstring?: string,
-        translationMaxLength?: number,
-        translationSubstringPositionInLine?: number
-    )
-    { 
-        this.id = id;
-        this.substringToTranslate = substringToTranslate;
-        this.description = description;
-        this.context = context;
-        this.id_fileOwner = id_fileOwner;
-        this.positionInFile = positionInFile;
-        this.originalString = originalString;
-        this.hasTranslationSubstring = hasTranslationSubstring;
-        this.translationSubstring = translationSubstring;
-        this.translationMaxLength = translationMaxLength;
-        this.translationSubstringPositionInLine = translationSubstringPositionInLine;
-    }
-    
 }
