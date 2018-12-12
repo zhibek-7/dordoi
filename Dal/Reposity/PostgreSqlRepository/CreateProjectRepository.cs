1929ce1d8f4bbbf1966cf07dbce07d7edbcc273f
Zhibek <jibek.070292@gmail.com>
2018-12-12 13:37:10 +0300

    Новые файлы для удаления проекта

---
+++ Dal/Reposity/PostgreSqlRepository/CreateProjectRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Context;
+
+namespace DAL.Reposity.PostgreSqlRepository
+{
+    class CreateProjectRepository
+    {
+        private PostgreSqlNativeContext context;
+
+        public CreateProjectRepository()
+        {
+            context = PostgreSqlNativeContext.getInstance();
+        }
+
+        public void Add( )
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
