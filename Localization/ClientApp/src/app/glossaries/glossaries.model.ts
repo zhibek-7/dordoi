90924696e6d971fef81f65df989924750dc0e824
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-12-14 18:29:58 +0400

    added shared.module with pagination.component (moved from glossaries module)

--- Localization/ClientApp/src/app/glossaries/glossaries.model.ts
+++ Localization/ClientApp/src/app/glossaries/glossaries.model.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { GlossariesRoutingModule } from 'src/app/glossaries/glossaries-routing.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { GlossariesService } from 'src/app/services/glossaries.service';
 import { PartsOfSpeechService } from 'src/app/services/partsOfSpeech.service';
@@ -17,7 +18,6 @@ import { AddTermFormComponent } from 'src/app/glossaries/components/add-term-for
 import { DeleteTermConfirmationComponent } from 'src/app/glossaries/components/delete-term-confirmation-modal/delete-term-confirmation-modal.component';
 import { DeleteTermsConfirmationComponent } from 'src/app/glossaries/components/delete-terms-confirmation-modal/delete-terms-confirmation-modal.component';
 import { EditTermFormComponent } from 'src/app/glossaries/components/edit-term-form-modal/edit-term-form-modal.component';
-import { PaginationComponent } from 'src/app/glossaries/components/pagination/pagination.component';
 import { SetLanguagesFormModalComponent } from 'src/app/glossaries/components/set-languages-form-modal/set-languages-form-modal.component';
 import { LanguageService } from 'src/app/services/languages.service';
 import { FilterSelectableLocalesPipe } from 'src/app/glossaries/pipes/filterSelectableLocales.pipe';
@@ -30,6 +30,7 @@ import { TermDetailsEditableComponent } from 'src/app/glossaries/components/term
     CommonModule,
     GlossariesRoutingModule,
     FormsModule,
+    SharedModule,
   ],
   declarations: [
     GlossariesComponent,
@@ -40,7 +41,6 @@ import { TermDetailsEditableComponent } from 'src/app/glossaries/components/term
     DeleteTermConfirmationComponent,
     DeleteTermsConfirmationComponent,
     EditTermFormComponent,
-    PaginationComponent,
     SetLanguagesFormModalComponent,
     SetLanguagesComponent,
     FilterSelectableLocalesPipe,
