3881e913c87b513862ec4d4f2894a10b6038574e
Gleb Starostin <gleb.starostin@coderlink.cc>
2018-12-17 12:59:35 +0400

    Glossaries: refactoring, cleanups

--- Dal/Reposity/PostgreSqlRepository/BaseRepository.cs
+++ Dal/Reposity/PostgreSqlRepository/BaseRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SqlKata;
 using Utilities.Logs;
 
 namespace DAL.Reposity.PostgreSqlRepository
@@ -22,7 +23,12 @@ namespace DAL.Reposity.PostgreSqlRepository
             this._logger.WriteLn($"Query {sql}, param: {param}");
         }
 
-        protected string DictionaryToString(Dictionary<string, object> dictionary)
+        protected void LogQuery(SqlResult sqlResult)
+        {
+            this._logger.WriteLn($"Query {sqlResult.Sql}, param: {this.DictionaryToString(sqlResult.NamedBindings)}");
+        }
+
+        private string DictionaryToString(Dictionary<string, object> dictionary)
         {
             var stringBuilder =
                 dictionary.SkipLast(1)
@@ -32,6 +38,35 @@ namespace DAL.Reposity.PostgreSqlRepository
             return dictionary.TakeLast(1)
                 .Select(pair => stringBuilder.Append($"{pair.Key} = {pair.Value} }}"))
                 .FirstOrDefault()?.ToString() ?? "null";
+        }
+
+        protected Query ApplyPagination(Query query, int offset, int limit)
+        {
+            if (offset > 0)
+            {
+                query = query.Skip(offset);
+            }
+            if (limit > 0)
+            {
+                query = query.Take(limit);
+            }
+            return query;
+        }
+
+        protected Query ApplySorting(Query query, Dictionary<string, string> columnNamesMappings, string[] sortBy, bool sortAscending)
+        {
+            if (sortBy.Any())
+            {
+                var columnNamesToSort = sortBy
+                    .Select(x => x.ToLower())
+                    .Distinct()
+                    .Where(x => columnNamesMappings.ContainsKey(x))
+                    .Select(x => columnNamesMappings[x])
+                    .ToArray();
+                if (columnNamesToSort.Any())
+                    query = sortAscending ? query.OrderBy(columnNamesToSort) : query.OrderByDesc(columnNamesToSort);
+            }
+            return query;
         }
 
     }
