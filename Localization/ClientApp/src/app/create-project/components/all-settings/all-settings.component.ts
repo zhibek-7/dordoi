0daf3e113065dd3e9a0909dd1f5544100f890a1f
Zhibek <jibek.070292@gmail.com>
2018-12-27 12:10:30 +0300

    изменила all-settings.component.ts

--- Localization/ClientApp/src/app/create-project/components/all-settings/all-settings.component.ts
+++ Localization/ClientApp/src/app/create-project/components/all-settings/all-settings.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit} from '@angular/core';
-
-
 import { ProjectsService } from '../../../services/projects.service';
-import { Project } from '../../../models/Project';
-
-
+import { Project } from '../../../models/Project/Project';
 import { FormControl} from '@angular/forms';
 
 
@@ -20,12 +16,18 @@ export class AllSettingsComponent implements OnInit {
   constructor(private projectsService: ProjectsService) { }
   currentProjectName = '';
   currentProjectDescription = '';
-  currentProjectPublic = '1';
-  currentProjectPrivate = '';
-  currentProjectFileTrue = '';
-  currentSkipUntranslStrTrue = '';
-  currentProjectExportTrue = '';
-  currentProjectNotificationTrue = '';
+  currentProjectPublic = true;
+  currentProjectPrivate = false;
+  currentProjectFileTrue = false;
+  currentSkipUntranslStrTrue = false;
+  currentProjectExportTrue = false;
+  currentProjectNotificationTrue = false;
+  currentProjectdateOfCreation =Date.now();
+  currentProjectID_SourceLocale = 1;
+  currentProjectlastActivity = Date.now();
+  currentProjectlogo = '';
+  currentProjectid = 1;
+  currentProjecturl = '';
 
   pjName = new FormControl();
   pjDescription = new FormControl();
@@ -36,6 +38,7 @@ export class AllSettingsComponent implements OnInit {
   pjExportTrue = new FormControl();
   pjNotificationTrue = new FormControl();
 
+
   ngOnInit() {
     this.currentProjectName = sessionStorage.getItem('ProjectName');
     console.log('ProjectName=' + sessionStorage.getItem('ProjectName'));
@@ -45,43 +48,63 @@ export class AllSettingsComponent implements OnInit {
     console.log('ProjectDescription=' + sessionStorage.getItem('ProjectDescription'));
     console.log('ProjectDescriptionID=' + sessionStorage.getItem('ProjectDescriptionID'));
 
-    this.currentProjectPublic = sessionStorage.getItem('currentProjectPublic');
-    console.log('ProjectPublic=' + sessionStorage.getItem('ProjectPublic'));
-    console.log('ProjectPublicID=' + sessionStorage.getItem('ProjectPublicID'));
+    //this.currentProjectPublic = sessionStorage.getItem('currentProjectPublic');
+    //console.log('ProjectPublic=' + sessionStorage.getItem('ProjectPublic'));
+    //console.log('ProjectPublicID=' + sessionStorage.getItem('ProjectPublicID'));
 
-    this.currentProjectPrivate = sessionStorage.getItem('currentProjectPrivate');
-    console.log('ProjectPrivate=' + sessionStorage.getItem('ProjectPrivate'));
-    console.log('ProjectPrivateID=' + sessionStorage.getItem('currentProjectPrivateID'));
+    //this.currentProjectPrivate = sessionStorage.getItem('currentProjectPrivate');
+    //console.log('ProjectPrivate=' + sessionStorage.getItem('ProjectPrivate'));
+    //console.log('ProjectPrivateID=' + sessionStorage.getItem('currentProjectPrivateID'));
 
-    this.currentProjectFileTrue = sessionStorage.getItem('currentProjectFileTrue');
-    console.log('ProjectFileTrue=' + sessionStorage.getItem('ProjectFileTrue'));
-    console.log('ProjectFileTrueID=' + sessionStorage.getItem('ProjectFileTrueID'));
+    //this.currentProjectFileTrue = sessionStorage.getItem('currentProjectFileTrue');
+    //console.log('ProjectFileTrue=' + sessionStorage.getItem('ProjectFileTrue'));
+    //console.log('ProjectFileTrueID=' + sessionStorage.getItem('ProjectFileTrueID'));
 
-    this.currentSkipUntranslStrTrue = sessionStorage.getItem('currentSkipUntranslStrTrue');
-    console.log('SkipUntranslStrTrue=' + sessionStorage.getItem('SkipUntranslStrTrue'));
-    console.log('SkipUntranslStrTrueID=' + sessionStorage.getItem('SkipUntranslStrTrueID'));
+    //this.currentSkipUntranslStrTrue = sessionStorage.getItem('currentSkipUntranslStrTrue');
+    //console.log('SkipUntranslStrTrue=' + sessionStorage.getItem('SkipUntranslStrTrue'));
+    //console.log('SkipUntranslStrTrueID=' + sessionStorage.getItem('SkipUntranslStrTrueID'));
 
-    this.currentProjectExportTrue = sessionStorage.getItem('currentProjectExportTrue');
-    console.log('ProjectExportTrue=' + sessionStorage.getItem('ProjectExportTrue'));
-    console.log('ProjectExportTrueID=' + sessionStorage.getItem('ProjectExportTrueID'));
+    //this.currentProjectExportTrue = sessionStorage.getItem('currentProjectExportTrue');
+    //console.log('ProjectExportTrue=' + sessionStorage.getItem('ProjectExportTrue'));
+    //console.log('ProjectExportTrueID=' + sessionStorage.getItem('ProjectExportTrueID'));
 
-    this.currentProjectNotificationTrue = sessionStorage.getItem('currentProjectNotificationTrue');
-    console.log('ProjectNotificationTrue=' + sessionStorage.getItem('ProjectNotificationTrue'));
-    console.log('ProjectNotificationTrueID=' + sessionStorage.getItem('ProjectNotificationTrueID'));
+    //this.currentProjectNotificationTrue = sessionStorage.getItem('currentProjectNotificationTrue');
+    //console.log('ProjectNotificationTrue=' + sessionStorage.getItem('ProjectNotificationTrue'));
+    //console.log('ProjectNotificationTrueID=' + sessionStorage.getItem('ProjectNotificationTrueID'));
 
   }
 
+  name?: string;
+  ableToDownload?: boolean;
+  ableToLeftErrors?: boolean;
+  dateOfCreation?: Date;
+  defaultString?: string;
+  description?: string;
+  ID_SourceLocale?: number;
+  lastActivity?: Date;
+  logo?: string;
+  notifyConfirm?: boolean;
+  notifyFinish?: boolean;
+  notifyNew?: boolean;
+  visibility?: boolean;
+  id?: number;
+  url?: string;
 
   addProject() {
     console.log("!!!=" + this.pjName.value);
-    console.log("!!!=" + this.pjDescription.value);
-    console.log("!!!=" + this.pjPublic.value);
-    console.log("!!!=" + this.pjPrivate.value);
-    console.log("!!!=" + this.pjFileTrue.value);
-    console.log("!!!=" + this.pjSkipUntranslStrTrue.value);
-    console.log("!!!=" + this.pjExportTrue.value);
-    console.log("!!!=" + this.pjNotificationTrue.value);
-    let newProject: Project = new Project(this.pjName.value, this.pjName.value, this.pjDescription.value);// поменять на id реального пользователя, когда появится
+
+    let newProject: Project = new Project(
+      this.pjName.value,
+      this.pjPublic.value,
+      this.pjPrivate.value,
+      this.pjFileTrue.value,
+      this.pjSkipUntranslStrTrue.value,
+      this.pjExportTrue.value,
+      this.pjNotificationTrue.value,
+    );
+
+
+    // поменять на id реального пользователя, когда появится
     this.projectsService.addProject(newProject);
 
   }
