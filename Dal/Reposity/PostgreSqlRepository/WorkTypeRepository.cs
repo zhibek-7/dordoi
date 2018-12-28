99a70daab77ec533fc5c304d46718da87fe63d51
Oleg Borodatov <oleg.borodatov@coderlink.cc>
2018-12-28 14:17:23 +0700

    Доработка модели и репозитория действий пользователей

--- Dal/Reposity/PostgreSqlRepository/WorkTypeRepository.cs
+++ Dal/Reposity/PostgreSqlRepository/WorkTypeRepository.cs
@@ -7,6 +7,23 @@ using Models.DatabaseEntities;
 
 namespace DAL.Reposity.PostgreSqlRepository
 {
+    public enum WorkTypes
+    {
+        Authorize = 1, //1	Авторизация пользователя                                                                            
+        Login, //2	Выход пользователя                                                                                  
+        CreateProject, //3	Создание проекта                                                                                    
+        AddFile, //4	Добавление файла                                                                                    
+        UpdateFile, //5	Изменение файла                                                                                     
+        AddString, //6	Добавлении строки                                                                                   
+        UpdateString, //7	Изменение строки                                                                                    
+        DeleteString, //8	Удаление строки                                                                                     
+        AddTraslation, //9	Добавление перевода                                                                                 
+        DeleteTranslation, //10	Удалении перевода                                                                                   
+        UpdateTranslation, //11	Изменение перевода                                                                                  
+        ConfirmTranslation, //12	Подтверждение перевода                                                                              
+        ChoseTranslation //13	Выбор перевода менеджером
+    }
+
     public class WorkTypeRepository : BaseRepository, IRepositoryAsync<WorkType>
     {
         private PostgreSqlNativeContext context;
--- Models/DatabaseEntities/UserAction.cs
+++ Models/DatabaseEntities/UserAction.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Text;
 
 namespace Models.DatabaseEntities
 {
@@ -21,34 +19,38 @@ namespace Models.DatabaseEntities
         [Required]
         public int  ID_worktype { get; set; }
         /// <summary>
+        /// Вид деятельности в виде строки
+        /// </summary>
+        public string Worktype { get; set; }
+        /// <summary>
         /// Время активности. Устанавливается в БД автоматически
         /// </summary>
         [Required]
         public DateTime Datetime { get; set; }
         /// <summary>
-        /// Описание.
+        /// Описание
         /// </summary>
         [Required]
         public string Description { get; set; }
         /// <summary>
         /// Ссылка на язык
         /// </summary>
-        public int  ID_Locale { get; set; }
+        public int?  ID_Locale { get; set; }
         /// <summary>
         /// Ссылка на файл (для регистрации активности по добавлению, удалению и редактированию файлов
         /// </summary>
-        public int ID_File { get; set; }
+        public int? ID_File { get; set; }
         /// <summary>
         /// Ссылка на строку (для регистрации активности по добавлению, удалению и редактированию строк
         /// </summary>
-        public int ID_String { get; set; }
+        public int? ID_String { get; set; }
         /// <summary>
         /// Ссылка на перевод (для регистрации активности по добавлению, удалению и редактированию переводов
         /// </summary>
-        public int ID_Translation { get; set; }
+        public int? ID_Translation { get; set; }
         /// <summary>
         /// Ссылка на перевод (для регистрации активности по добавлению, удалению и редактированию проектов локализации
         /// </summary>
-        public int ID_Project { get; set; }
+        public int? ID_Project { get; set; }
     }
 }
