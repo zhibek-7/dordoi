6a77d244d595eef670f5b05f6d11d6e3b7a9b0c4
Gleb Starostin <gleb.starostin@coderlink.cc>
2019-01-11 11:44:40 +0400

    exception message added to response for web app common exception handler

--- Localization/Startup.cs
+++ Localization/Startup.cs
@@ -14,6 +14,7 @@ using Models.Services;
 using Microsoft.AspNetCore.Diagnostics;
 using Utilities.Logs;
 using System.Net;
+using Utilities.Extensions;
 
 namespace Localization
 {
@@ -88,6 +89,7 @@ namespace Localization
                     var unhandledException = errorFeature?.Error;
                     this._exceptionLog.WriteLn("Localization web app unhandled exception.", unhandledException);
                     context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    await context.Response.Body.WriteAsync(unhandledException?.Message ?? string.Empty);
                 }));
 
             if (!env.IsDevelopment())
