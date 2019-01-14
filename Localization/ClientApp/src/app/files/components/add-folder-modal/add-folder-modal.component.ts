b2bebf7a70d14555a15d01d853c7fc112c687d26
Gleb Starostin <gleb.starostin@coderlink.cc>
2019-01-14 17:15:52 +0400

    add folder modal implemented

---
+++ Localization/ClientApp/src/app/files/components/add-folder-modal/add-folder-modal.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+import { ModalComponent } from 'src/app/shared/components/modal/modal.component';
+import { File } from 'src/app/models/database-entities/file.type';
+
+@Component({
+  selector: 'app-add-folder-modal',
+  templateUrl: './add-folder-modal.component.html',
+  styleUrls: ['./add-folder-modal.component.css']
+})
+export class AddFolderModalComponent extends ModalComponent implements OnInit {
+
+  newFolder: File;
+
+  @Output() newFolderSubmitted = new EventEmitter<File>();
+
+  constructor() {
+    super();
+    this.resetNewFileModel();
+  }
+
+  ngOnInit() {
+  }
+
+  show() {
+    this.resetNewFileModel();
+    super.show();
+  }
+
+  submitNewFolder() {
+    this.hide();
+    this.newFolderSubmitted.emit(this.newFolder);
+  }
+
+  resetNewFileModel() {
+    this.newFolder = new File();
+    this.newFolder.isFolder = true;
+    this.newFolder.name = '';
+  }
+
+}
