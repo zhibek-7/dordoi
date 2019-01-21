0ff024ff99104f35e6c4fea8f79877d3741b2fe0
Oleg Borodatov <oleg.borodatov@coderlink.cc>
2019-01-21 09:40:41 +0700

    Update userAction model

--- Models/DatabaseEntities/UserAction.cs
+++ Models/DatabaseEntities/UserAction.cs
@@ -14,6 +14,11 @@ namespace Models.DatabaseEntities
         [Required]
         public int ID_User { get; set; }
         /// <summary>
+        /// Имя пользователя совершившего действие
+        /// </summary>
+        [Required]
+        public string User { get; set; }
+        /// <summary>
         /// ИД вида деятельности 
         /// </summary>
         [Required]
@@ -21,6 +26,7 @@ namespace Models.DatabaseEntities
         /// <summary>
         /// Вид деятельности в виде строки
         /// </summary>
+        [Required]
         public string Worktype { get; set; }
         /// <summary>
         /// Время активности. Устанавливается в БД автоматически
@@ -37,22 +43,41 @@ namespace Models.DatabaseEntities
         /// </summary>
         public int? ID_Locale { get; set; }
         /// <summary>
+        /// Язык
+        /// </summary>
+        public string Locale { get; set; }
+        /// <summary>
         /// Ссылка на файл (для регистрации активности по добавлению, удалению и редактированию файлов
         /// </summary>
         public int? ID_File { get; set; }
+        /// <summary>
+        /// Название файла (для регистрации активности по добавлению, удалению и редактированию файлов
+        /// </summary>
+        public string File { get; set; }
         /// <summary>
         /// Ссылка на строку (для регистрации активности по добавлению, удалению и редактированию строк
         /// </summary>
         public int? ID_String { get; set; }
         /// <summary>
+        /// Содержимое строки (для регистрации активности по добавлению, удалению и редактированию строк
+        /// </summary>
+        public string String { get; set; }
+        /// <summary>
         /// Ссылка на перевод (для регистрации активности по добавлению, удалению и редактированию переводов
         /// </summary>
         public int? ID_Translation { get; set; }
         /// <summary>
-        /// Ссылка на перевод (для регистрации активности по добавлению, удалению и редактированию проектов локализации
+        /// Содержимое перевода (для регистрации активности по добавлению, удалению и редактированию переводов
+        /// </summary>
+        public string Translation { get; set; }
+        /// <summary>
+        /// Ссылка на проект (для регистрации активности по добавлению, удалению и редактированию проектов локализации
         /// </summary>
         public int? ID_Project { get; set; }
-
+        /// <summary>
+        /// Название проекта (для регистрации активности по добавлению, удалению и редактированию проектов локализации
+        /// </summary>
+        public string Project { get; set; }
 
         public UserAction() { }
 
