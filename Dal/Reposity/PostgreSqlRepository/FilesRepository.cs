4dc4a0a3a827475786b01c3718f3418441c240c2
Mashchitskiy_IE <igor.maschitskiy@coderlink.cc>
2019-01-21 15:24:29 +0500

    FilesRepository.Load now support LocalizationProject.AbleToLeftErrors property.

--- Dal/Reposity/PostgreSqlRepository/FilesRepository.cs
+++ Dal/Reposity/PostgreSqlRepository/FilesRepository.cs
@@ -364,7 +364,19 @@ namespace DAL.Reposity.PostgreSqlRepository
                         {
                             var sqlTranslationQuery = string.Format("SELECT * FROM \"Translations\" WHERE \"ID_String\" = @id_translationSubstring AND \"ID_Locale\" = @id_locale{0} SORT BY \"Selected\" DESC, \"Confirmed\" DESC, \"DateTime\" DESC LIMIT 1", localizationProject.export_only_approved_translations ? " AND \"Confirmed\" = true" : "");
                             var translation = await connection.QuerySingleOrDefaultAsync<Translation>(sqlTranslationQuery, new { translationSubstrings[i].ID, id_locale });
-                            if (translation == null && localizationProject.original_if_string_is_not_translated) continue;
+                            if (translation == null)
+                            {
+                                if (localizationProject.AbleToLeftErrors)
+                                {
+                                    int n = translationSubstrings[i].PositionInText;
+                                    while (n != 0 && (output[n - 1] != '\n' || output[n - 1] != '\r')) n--;
+                                    int m = output.IndexOfAny(new char[] { '\r', '\n' }, translationSubstrings[i].PositionInText);
+                                    while (m != output.Length - 1 && output[m + 1] != '\n') m++;
+                                    if (n > (i == 0 ? -1 : translationSubstrings[i - 1].PositionInText) && m < (i == translationSubstrings.Count - 1 ? output.Length : translationSubstrings[i + 1].PositionInText)) output = output.Remove(n, m - n + 1);
+                                    continue;
+                                }
+                                if (localizationProject.original_if_string_is_not_translated) continue;
+                            }
                             output = output.Remove(translationSubstrings[i].PositionInText, translationSubstrings[i].Value.Length).Insert(translationSubstrings[i].PositionInText, translation == null ? localizationProject.DefaultString : translation.Translated);
                         }
                         
