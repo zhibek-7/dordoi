e465b23b2f0f08e50ad8b3a74b733e16321f0103
Gleb Starostin <gleb.starostin@coderlink.cc>
2019-02-06 13:17:32 +0400

    refactoring: added Async postfix to async methods

--- Models/Interfaces/Repository/IFilesRepository.cs
+++ Models/Interfaces/Repository/IFilesRepository.cs
@@ -6,16 +6,16 @@ namespace Models.Interfaces.Repository
 {
     public interface IFilesRepository : IRepositoryAsync<File>
     {
-        Task<bool> Upload(File item);
+        Task<bool> UploadAsync(File item);
         Task<IEnumerable<File>> GetByProjectIdAsync(int projectId);
         Task<IEnumerable<File>> GetByProjectIdAsync(int projectId, string fileNamesSearch);
-        Task<File> GetLastVersionByNameAndParentId(string name, int? parentId);
+        Task<File> GetLastVersionByNameAndParentIdAsync(string name, int? parentId);
         IEnumerable<File> GetInitialFolders(int projectId);
         Task ChangeParentFolderAsync(int fileId, int? newParentId);
         Task AddTranslationLocalesAsync(int fileId, IEnumerable<int> localesIds);
         Task<IEnumerable<Locale>> GetLocalesForFileAsync(int fileId);
         Task DeleteTranslationLocalesAsync(int fileId);
-        Task<string> GetFileContent(int id, int id_locale = -1);
+        Task<string> GetFileContentAsync(int id, int id_locale = -1);
         Task<IEnumerable<FileTranslationInfo>> GetFileTranslationInfoByIdAsync(int fileId);
         Task<IEnumerable<File>> GetFilesByParentFolderIdAsync(int parentFolderId);
     }
