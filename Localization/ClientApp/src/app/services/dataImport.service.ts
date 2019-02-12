36b8402eb8d7f63e007a24bd647f82131c7e98dd
Gleb Starostin <gleb.starostin@coderlink.cc>
2019-02-12 15:01:25 +0400

    cleanups

--- Localization/ClientApp/src/app/services/dataImport.service.ts
+++ Localization/ClientApp/src/app/services/dataImport.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Locale } from '../models/database-entities/locale.type';
 
 @Injectable({
   providedIn: 'root'
