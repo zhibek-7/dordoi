8b6a1aaefaafcc43c2e92152ef7ddaa553b3a484
Zhibek <zhibek.naamatova@coderlink.cc>
2019-04-08 14:54:49 +0700

    //csv file

---
+++ Models/DatabaseEntities/PartialEntities/Translation/TranslationSubstringPartOfSpeech.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DatabaseEntities.PartialEntities.Translation
+{
+    [Serializable]
+    public class TranslationSubstringPartOfSpeech
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid id_glossary { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid id_part_of_speech { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid id_string { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid t_id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public Guid t_id_locale { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ts_description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ts_substring_to_translate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ps_name_text { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string t_translated { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string l_code { get; set; }
+
+    }
+}
---
+++ Models/DatabaseEntities/localization_projects_glossaries.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.DatabaseEntities
+{
+    [Serializable]
+    public class localization_projects_glossaries
+    {
+        public Guid id_localization_project { get; set; }
+        public Guid id_glossary { get; set; }
+    }
+}
